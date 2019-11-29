bf59f54ea21166b4ea6d0fb1c3f0d30147743439
Vincent Mackay <mackv42@gmail.com>
2019-11-29 16:08:09 -0600

    init pricing example

---
+++ HeadHunter-Hunter/Models/PricingOption.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HeadHunter_Hunter.Models
+{
+    public class PricingOption
+    {
+        public PricingOption(string name, double price)
+        {
+            this.Name = name;
+            this.Price = price;
+        }
+        public string Name;
+        public double Price;
+    }
+}
