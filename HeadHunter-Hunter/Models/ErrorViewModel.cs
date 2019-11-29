8cb2932a4dfbc64af31274aa6e482874bd32625d
Vincent Mackay <mackv42@gmail.com>
2019-11-29 13:31:11 -0600

    init with visual studio

---
+++ HeadHunter-Hunter/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HeadHunter_Hunter.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
