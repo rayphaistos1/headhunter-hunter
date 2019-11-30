749f945b442da043cf9b48818174e72c80ea4801
Vincent Mackay <mackv42@gmail.com>
2019-11-29 18:40:06 -0600

    update links

--- HeadHunter-Hunter/Data/Migrations/20191130002941_init.Designer.cs
+++ HeadHunter-Hunter/Data/Migrations/20191130002941_init.Designer.cs
@@ -4,14 +4,16 @@ using HeadHunter_Hunter.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace HeadHunter_Hunter.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20191130002941_init")]
+    partial class init
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
